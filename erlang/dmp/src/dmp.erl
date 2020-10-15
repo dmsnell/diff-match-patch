84fb061daf45b6bf4982569a6f4e7a72712c22c9
Dennis Snell <dennis.snell@automattic.com>
2020-10-15 02:58:13 -0700

    Refactor

--- erlang/dmp/src/dmp.erl
+++ erlang/dmp/src/dmp.erl
@@ -2,52 +2,39 @@
 -author("Dennis Snell <dennis.snell@automattic.com>").
 
 -include_lib("eunit/include/eunit.hrl").
+
 -define(S(X), <<X/utf16>>).
 
 %% Interface functions
 
 %% Internal functions
 
--spec common_prefix(binary(), binary()) -> non_neg_integer().
+-spec common_prefix(Left :: binary(), Right :: binary()) -> non_neg_integer().
 
 common_prefix(Left, Right) ->
-    cp_backup(
-        Left,
-        byte_size(Left) div 2,
-        binary:longest_common_prefix([Left, Right]) div 2
+    cp_surrogate_shift(
+        binary:longest_common_prefix([Left, Right]) div 2,
+        char_type(suffix(Left, 2))
     ).
 
-cp_backup(_Left, _LeftLength, 0) -> 0;
-cp_backup(_Left, Length, Length) -> Length;
-cp_backup(Left, _LeftLength, CodeUnits) ->
-    Last = binary:part(Left, (CodeUnits - 1) * 2, 2),
-    case is_high_surrogate(Last) of
-        true  -> CodeUnits - 1;
-        false -> CodeUnits
-    end.
+cp_surrogate_shift(CodeUnits, high_surrogate) -> CodeUnits - 1;
+cp_surrogate_shift(CodeUnits, _)              -> CodeUnits.
 
--spec common_suffix(binary(), binary()) -> non_neg_integer().
+-spec common_suffix(Left :: binary(), Right :: binary()) -> non_neg_integer().
 
 common_suffix(Left, Right) ->
-    cs_backup(
-        Left,
-        byte_size(Left) div 2,
-        binary:longest_common_suffix([Left, Right]) div 2
+    cs_surrogate_shift(
+        binary:longest_common_suffix([Left, Right]) div 2,
+        char_type(prefix(Left, 2))
     ).
 
-cs_backup(_Left, _LeftLength, 0) -> 0;
-cs_backup(_Left, Length, Length) -> Length;
-cs_backup(Left, LeftLength, CodeUnits) ->
-    First = binary:part(Left, (LeftLength - CodeUnits) * 2, 2),
-    case is_low_surrogate(First) of
-        true  -> CodeUnits - 1;
-        false -> CodeUnits
-    end.
+cs_surrogate_shift(CodeUnits, low_surrogate) -> CodeUnits + 1;
+cs_surrogate_shift(CodeUnits, _)             -> CodeUnits.
 
--spec common_overlap(binary(), binary()) -> non_neg_integer().
+-spec common_overlap(Left :: binary(), Right :: binary()) -> non_neg_integer().
 
-common_overlap(Left, Right) when byte_size(Left) == 0; byte_size(Right) == 0 -> 0;
-common_overlap(Left, Right) -> common_overlap(Left, Right, 0, 2) div 2.
+common_overlap(Left, Right) ->
+    common_overlap(Left, Right, 0, 2) div 2.
 
 common_overlap(Left, Right, Best, Length)
     when byte_size(Left) =< Length; byte_size(Right) =< Length ->
@@ -70,8 +57,9 @@ co_scan(Left, Right, Best, Length, {P, _Suffix, _Prefix}) ->
 prefix(Binary, Bytes) -> binary:part(Binary, 0, Bytes).
 suffix(Binary, Bytes) -> binary:part(Binary, byte_size(Binary), -Bytes).
 
-is_high_surrogate(<<A:16>>) -> A >= 16#D800 andalso A =< 16#DBFF.
-is_low_surrogate(<<A:16>>) -> A >= 16#DC00 andalso A =< 16#DFFF.
+char_type(<<A:16>>) when A >= 16#D800, A =< 16#DBFF -> high_surrogate;
+char_type(<<A:16>>) when A >= 16#DC00, A =< 16#DFFF -> low_surrogate;
+char_type(<<_:16>>)                                 -> normal.
 
 %% EUnit tests
 -ifdef(EUNIT).
